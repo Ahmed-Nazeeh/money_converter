7928bcffe944b9b32dd114e4a20c3eb0abc7de02
Ahmed-Nazeeh <aa.nazeeh@gmail.com>
2022-09-12 16:51:29 +0200

    add devise for registration

---
+++ features/step_definitions/user_registration_steps.rb
@@ -0,0 +1,23 @@
+Given('Iam on the homepage') do
+    visit "/"
+end
+
+When('I click on the rigestration link') do
+click_on "registration-link"
+end
+
+When('I fill in and submit the registration form') do
+    fill_in "user_name", with: "ahmed"
+    fill_in "user_email", with: "ahmed@example.com"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_on "user_registration_submit"
+end
+
+Then('should see a registration confirmation message') do
+    expect(page).to have_content("Welcome")
+end
+
+Then('I should receive a confirmation email') do
+    step %{"ahmed@example.com" should receive an email}
+end
---
+++ features/user_registration.feature
@@ -0,0 +1,11 @@
+Feature: User registration
+         As a first-time visitor
+         I want to be able to create an account
+         so that I can access the members area
+
+         Scenario: I create an account
+         Given Iam on the homepage
+         When I click on the rigestration link
+         And I fill in and submit the registration form
+         Then should see a registration confirmation message
+         And I should receive a confirmation email
